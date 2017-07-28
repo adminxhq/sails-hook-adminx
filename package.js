cb7d244a8542243c43d3b5eb24a8f85849dd3453
Albert Peiró <acordonet@gmail.com>
2017-07-28 19:40:30 +0200

    Initial commit

---
+++ package.js
@@ -0,0 +1,9 @@
+{
+    "name": "sails-hook-adminx",
+    "version": "0.0.0",
+    "description": "a brief description of your hook",
+    "main": "index.js",
+    "sails": {
+        "isHook": true
+    }
+}
